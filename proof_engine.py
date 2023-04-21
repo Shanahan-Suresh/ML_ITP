02657bb829bcf3fe614502e6381b9918db0b758d
Shanahan <SHANAHAN.SURESH1@GMAIL.COM>
2023-04-21 04:41:52 +0100

    Implement a basic search and a DFS search engine

---
+++ proof_engine.py
@@ -0,0 +1,86 @@
+from inference_rules import *
+import itertools
+
+# List of all inference rules
+all_inference_rules = [
+    ('modus_ponens', apply_modus_ponens),
+    ('modus_tollens', apply_modus_tollens),
+    ('hypothetical_syllogism', apply_hypothetical_syllogism),
+    ('disjunctive_syllogism', apply_disjunctive_syllogism),
+    ('addition', apply_addition),
+    ('simplification', apply_simplification),
+    #('resolution', apply_resolution),
+    ('associativity_add', apply_associativity_add),
+    #('associativity_mul', apply_associativity_mul),
+    #('commutativity_add', apply_commutativity_add),
+    #('commutativity_mul', apply_commutativity_mul),
+    #('distributivity', apply_distributivity),
+    #('identity_add', apply_identity_add),
+    #('identity_mul', apply_identity_mul),
+    #('inverse_add', apply_inverse_add),
+    #('inverse_mul', apply_inverse_mul),
+]
+
+# Function to carry out specific inference rules
+def apply_rule(premise1, premise2, rule):
+    rule_name, rule_func = rule
+    return rule_func(premise1, premise2)
+
+def search_inference_rules(premise1, premise2):
+    for rule_name, rule_func in all_inference_rules:
+        result = apply_rule(premise1, premise2, (rule_name, rule_func))
+        if result:
+            return rule_name, result
+    return None, None
+
+def dfs_search_inference_rules(premises, conclusion, depth=0, max_depth=5, visited=None, applied_rules=None):
+    if visited is None:
+        visited = set()
+        applied_rules = []
+
+    premises_tuple = tuple(premises)
+    if premises_tuple in visited:
+        return None, []
+
+    visited.add(premises_tuple)
+
+    for rule_name, rule_func in all_inference_rules:
+        if getattr(rule_func, 'single_premise', False):
+            for premise in premises:
+                result = rule_func(premise)
+                if result:
+                    #print('Result : {}\nIntended Conlusion: {}'.format(result, conclusion))
+                    if result == conclusion:
+                        return applied_rules + [rule_name], [result]
+
+                    if depth < max_depth:
+                        next_applied_rules, next_result = dfs_search_inference_rules(
+                            premises.union({result}), conclusion, depth + 1, max_depth, visited, applied_rules + [rule_name]
+                        )
+                        if next_result:
+                            return next_applied_rules, [result] + next_result
+        else:
+            for premise1 in premises:
+                for premise2 in premises:
+                    if premise1 == premise2:
+                        continue
+
+                    result = apply_rule(premise1, premise2, (rule_name, rule_func))
+
+                    if result:
+                        if result == conclusion:
+                            return applied_rules + [rule_name], [result]
+
+                        if depth < max_depth:
+                            next_applied_rules, next_result = dfs_search_inference_rules(
+                                premises.union({result}), conclusion, depth + 1, max_depth, visited, applied_rules + [rule_name]
+                            )
+                            if next_result:
+                                return next_applied_rules, [result] + next_result
+
+    return None, []
+
+
+
+
+
---
+++ proof_engine_test.py
@@ -0,0 +1,106 @@
+﻿from proof_engine import *
+
+def search_test():
+    A = Variable("A")
+    B = Variable("B")
+    C = Variable("C")
+
+    # Test Modus Ponens
+    premise1 = BinaryOp(A, Connective.IMPLIES, B)
+    premise2 = A
+    rule_name, result = dfs_search_inference_rules(premise1, premise2)
+    print(f"Applied rule: {rule_name}, Result: {result}")
+
+    # Test Modus Tollens
+    premise1 = BinaryOp(A, Connective.IMPLIES, B)
+    premise2 = UnaryOp(Connective.NOT, B)
+    rule_name, result = search_inference_rules(premise1, premise2)
+    print(f"Applied rule: {rule_name}, Result: {result}")
+
+    # Test HS
+    premise1 = BinaryOp(A, Connective.IMPLIES, B)
+    premise2 = BinaryOp(B, Connective.IMPLIES, C)
+    rule_name, result = search_inference_rules(premise1, premise2)
+    print(f"Applied rule: {rule_name}, Result: {result}")
+
+    # Test Addition
+    premise1 = A
+    premise2 = BinaryOp(A, Connective.OR, B)
+    rule_name, result = search_inference_rules(premise1, premise2)
+    print(f"Applied rule: {rule_name}, Result: {result}")
+
+    # Test DS
+    premise1 = BinaryOp(A, Connective.OR, B)
+    premise2 = UnaryOp(Connective.NOT, A)
+    rule_name, result = search_inference_rules(premise1, premise2)
+    print(f"Applied rule: {rule_name}, Result: {result}")
+
+    # Test Simplification
+    premise1 = BinaryOp(A, Connective.AND, B)
+    premise2 = A
+    rule_name, result = search_inference_rules(premise1, premise2)
+    print(f"Applied rule: {rule_name}, Result: {result}")
+
+def dfs_search_test():
+    A = Variable('A')
+    B = Variable('B')
+    C = Variable('C')
+    D = Variable('D')
+    E = Variable('E')
+
+    # Modus Pollus
+    premise1 = BinaryOp(A, Connective.IMPLIES, B)
+    premise2 = A
+    conclusion = B
+
+    # Call the dfs_search_inference_rules function
+    result = dfs_search_inference_rules({premise1, premise2}, conclusion)
+
+    # Print the result
+    print("Result:", result)
+
+    # MP and DS
+    premise1 = BinaryOp(A, Connective.IMPLIES, B)
+    premise2 = A
+    premise3 = BinaryOp(B, Connective.OR, C)
+    premise4 = UnaryOp(Connective.NOT, B)
+
+    conclusion = C
+
+    # Call the dfs_search_inference_rules function
+    result = dfs_search_inference_rules({premise1, premise2, premise3, premise4}, conclusion)
+
+    # Print the result
+    print("Result:", result)
+
+    expr1 = BinaryOp(BinaryOp(A, Connective.ADD, B), Connective.ADD, C)
+
+    # Conclusion
+    conclusion = BinaryOp(A, Connective.ADD, BinaryOp(B, Connective.ADD, C))
+
+    premises = {expr1}
+    applied_rules, result = dfs_search_inference_rules(premises, conclusion)
+
+    print("Applied Rules:", applied_rules)
+    print("Result:", result)
+
+    # 2 Associativity Add
+    expr1 = BinaryOp(BinaryOp(A, Connective.ADD, B), Connective.ADD, BinaryOp(C, Connective.ADD, D))
+    expr2 = BinaryOp(BinaryOp(C, Connective.ADD, D), Connective.ADD, E)
+
+    # Conclusion
+    conclusion = BinaryOp(A, Connective.ADD, BinaryOp(B, Connective.ADD, BinaryOp(C, Connective.ADD, BinaryOp(D, Connective.ADD, E))))
+
+    # Run the DFS search
+    #applied_rules, resulting_exprs = dfs_search_inference_rules({expr1, expr2}, conclusion)
+
+    # Check the result
+    #print("Applied rules:", applied_rules)
+    #print("Resulting expressions:", resulting_exprs)
+
+
+def main():
+    #search_test()
+    dfs_search_test()
+
+main()
